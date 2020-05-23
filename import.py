64506189583d4c79b34d5988618b24770ecf39a9
szympatka00 <65233797+szympatka00@users.noreply.github.com>
2020-05-23 12:00:49 +0200

    Added files for importing from books.csv

--- import.py
+++ import.py
@@ -3,6 +3,7 @@ import csv
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
+from flask_sqlalchemy import SQLAlchemy
 
 engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
@@ -10,13 +11,10 @@ db = scoped_session(sessionmaker(bind=engine))
 def main():
     b = open("books.csv")
     reader = csv.reader(b)
-    books = table ( #jak stworzyć table
 
-    )
     for isbn, title, author, year in reader:
         db.execute("INSERT INTO books (isbn, title, author, year) VALUES (:isbn, :title, :author, :year)",
                       {"isbn": isbn, "title": title, "author": author, "year": year})
-        print(f"Added books: isbn {isbn}, title: {title}")
     db.commit()
 
 if __name__ == "__main__":
---
+++ models.py
@@ -0,0 +1,10 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Book(db.Model):
+    __tablename__ = "books"
+    isbn =  db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=Flase)
+    author = db.Column(db.String, nullable=False)
+    year = db.Column(db.Integer, nullable=False)
