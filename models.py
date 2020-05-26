c28ec9afc291149e9828435fa01a7270d54050c9
szympatka00 <65233797+szympatka00@users.noreply.github.com>
2020-05-26 20:49:17 +0200

    Typo correction

--- models.py
+++ models.py
@@ -5,6 +5,6 @@ db = SQLAlchemy()
 class Book(db.Model):
     __tablename__ = "books"
     isbn =  db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String, nullable=Flase)
+    title = db.Column(db.String, nullable=False)
     author = db.Column(db.String, nullable=False)
     year = db.Column(db.Integer, nullable=False)
