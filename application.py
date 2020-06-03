2e115c3fb21bf29777cb8845a2fdf8fce5fb77a7
szympatka00 <65233797+szympatka00@users.noreply.github.com>
2020-06-03 15:57:37 +0200

    Added render_template and request libs

--- application.py
+++ application.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, session
+from flask import Flask, session, render_template, request
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
